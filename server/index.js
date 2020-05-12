cfac98ebf64292b7a0ca3333b1e9ccb131ad7313
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-11 21:13:34 -0400

    Set up passport

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hi:'there'})
-});
+// Tells passport a new strategy is available
+// instantiates the new strategy and tells it how to use it
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
+
+
 
 
