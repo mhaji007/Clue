14016393b252b3ef4324c8883b7f2e46fe39da11
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-11 22:48:31 -0400

    Test oauth implementation

--- server/index.js
+++ server/index.js
@@ -8,12 +8,21 @@ const app = express();
 // Tells passport a new strategy is available
 // instantiates the new strategy and tells it how to use it
 passport.use(
-    new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret   ,
-    callbackURL:'/auth/google/callback'
-    }, (accessToken) => console.log(accessToken))
+    new GoogleStrategy(
+        {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL:'/auth/google/callback'
+        },
+        accessToken => {
+            console.log(accessToken);
+        }
+    )
 );
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}))
 
 const PORT = process.env.PORT || 5000;
 
