6e601646fc85f8d2208bf2241501122ab10a7fe4
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-11 22:57:29 -0400

    Add authentication callback route handler

--- server/index.js
+++ server/index.js
@@ -22,7 +22,11 @@ passport.use(
 
 app.get('/auth/google', passport.authenticate('google', {
     scope: ['profile', 'email']
-}))
+    })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'))
+
 
 const PORT = process.env.PORT || 5000;
 
