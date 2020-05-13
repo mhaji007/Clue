7176853812e2eee5f9f139e4e98de44805e12b89
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-13 18:31:02 -0400

    Add passport callbacks

--- server/services/passport.js
+++ server/services/passport.js
@@ -19,10 +19,10 @@ passport.use(
             then((existingUser) => {
                 if(existingUser) {
                     // we already have a record with the given profile ID
+                    done(null, existingUser);
                 } else {
                     // we don't have a user record with this ID, make a new record
-                    new User({googleId: profile.id}).save();
-
+                    new User({googleId: profile.id}).save().then(user=>done(null, user));
                 }
             })
         }
