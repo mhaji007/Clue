bf482203224da1711c7407d8c414d3c94d8b81f0
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-13 03:04:07 -0400

    Set up MongoDB. Connect Mongoose to Mongo

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
 
+mongoose.connect(keys.mongoURI, {
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
 const app = express();
 
 require('./routes/authRoutes')(app);
 
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
-
-
-
-
