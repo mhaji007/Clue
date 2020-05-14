50b8cc91bf3a3bfd1ab1e7aab30cdbfb22b388bb
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-14 16:38:33 -0400

    Tweak google strategy settings

--- server/services/passport.js
+++ server/services/passport.js
@@ -29,7 +29,8 @@ passport.use(
         {
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL:'/auth/google/callback'
+        callbackURL:'/auth/google/callback',
+        proxy: true
         },
         (accessToken, refreshToken, profile, done) => {
             // create a user record in database
