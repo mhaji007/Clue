dcb99ff4d1f786d2d22190ca7cff52f949f22e4d
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-13 22:11:19 -0400

    Enable cookies

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -10,7 +12,20 @@ mongoose.connect(keys.mongoURI, {
     useNewUrlParser: true,
     useFindAndModify: false
 });
+
 const app = express();
+
+// Tells express to make use of cookies
+app.use(
+    //  configuration object
+    cookieSession({
+        maxAge:30*24*60*60*1000, // 30 days in miliseconds
+        keys: [keys.cookieKey] // keys to encrypt cookies
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
