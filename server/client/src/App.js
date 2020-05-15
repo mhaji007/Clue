2e928624c4f55ff90f8171efe18dad755f75b617
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-14 20:54:08 -0400

    Set up proxy for routing

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h2>Hi There</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <a href="/auth/google">Sign in with Google </a>
     </div>
   );
 }
