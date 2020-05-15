f8d4b27a20fd6c4b703e4329eec6e037712795ed
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-15 15:09:37 -0400

    Set up Redux

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,4 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
 
-ReactDOM.render();
+import App from './components/App';
+
+// first argument -> root reducer
+// second argument -> Store seed (important for server side rendering)
+// third argument -> middlewares
+const store = createStore(()=>[], {}, applyMiddleware())
+
+ReactDOM.render(
+<Provider store={store}>
+<App/>
+</Provider> , document.querySelector('#root'));
