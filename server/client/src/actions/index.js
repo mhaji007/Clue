944c2d184c6e22d547ae0b39389301d2721a2397
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-15 20:38:42 -0400

    Refactor fetchUser using async/await

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -4,10 +4,8 @@ import {FETCH_USER} from './types'
 // Produce an action and utilize redux-thunk
 // to manually send the result of the async 
 // action to the reducers 
-export const fetchUser = ( ) => {
-    // Wait for the async action to complete and
-    // only then dispatch the action to the reducers
-    return function (dispatch) {
-        axios.get('/api/current_user').then(res=>dispatch({type:FETCH_USER, payload:res}));
-    }
-}
+export const fetchUser = ( ) => async dispatch => {
+       const res = await axios.get('/api/current_user');
+    dispatch({type:FETCH_USER, payload:res});
+    
+};
