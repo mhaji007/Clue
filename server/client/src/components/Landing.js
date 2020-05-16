994b3b588d7814708df75e9eedd3b1bb29400675
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-16 01:43:16 -0400

    Structure landing component

---
+++ server/client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Capture from '../Capture.PNG'
+
+ const Landing = () => {
+    return (
+        <div style={{textAlign: 'center'}}>
+
+            <img src={Capture} alt="Clue" style={{textAlign: 'center', marginTop:'12px'}}/>
+        </div>
+
+    );
+};
+
+export default Landing;
