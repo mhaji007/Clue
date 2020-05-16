d69845829300a885d304cf4c6f8849763bc6cda7
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-15 22:08:14 -0400

    Add header content

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -7,12 +7,16 @@ class Header extends Component {
     renderContent() {
         switch (this.props.auth) {
             case null:
-                return 'Still deciding';
+                return;
             case false:
-                return 'Im logged out';
+                return (
+                    <li>
+                        <a href="/auth/google">Login With Google</a>
+                    </li>
+                )
                 
             default:
-                return 'Im logged in';
+                return <li> <a> Logout</a> </li>
         }
     }
 
