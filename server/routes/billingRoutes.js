281a5ad2f235e4553e25e83b5f51b5006b25f51b
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-17 13:39:59 -0400

    Create a charge object

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -3,7 +3,11 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
     app.post('/api/stripe', (req, res) => {
-
-
-    });
+            stripe.charges.create({
+                amount:500,
+                currency:'usd',
+                description: '$5 for 5 credits',
+                source:req.body.id
+            })
+        });
 };
