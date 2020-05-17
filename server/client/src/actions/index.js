7db694d72a4398d408e3af0a3cde66b153ce3562
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-16 21:20:15 -0400

    Posit the Stripe token

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -5,7 +5,7 @@ import {FETCH_USER} from './types'
 // to manually send the result of the async 
 // action to the reducers 
 export const fetchUser = ( ) => async dispatch => {
-       const res = await axios.get('/api/current_user');
+    const res = await axios.get('/api/current_user');
     dispatch({type:FETCH_USER, payload:res.data});
     
 };
--- server/client/src/components/Payments.js
+++ server/client/src/components/Payments.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 import StripeCheckout from 'react-stripe-checkout';
+import {connect} from 'react-redux';
+import * as actions from '../actions'
 
 class Payments extends Component {
     
@@ -9,7 +11,7 @@ class Payments extends Component {
             name="Clue"
             description="$5 for 5 email credits"
             amount={500}
-            token={token=>console.log(token)}
+            token={token=>this.props.handleToken(token)}
             stripeKey={process.env.REACT_APP_STRIPE_KEY}
             >
             <a className="btn purple darken-4">
@@ -21,4 +23,4 @@ class Payments extends Component {
     }
 }
  
-export default Payments;
+export default connect(null, actions)(Payments);
