60953fec9c2a8d59487bb901b947ef5a58572ba0
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-18 03:30:32 -0400

    Set up routing in production

--- server/index.js
+++ server/index.js
@@ -34,6 +34,22 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if(process.env.NODE_ENV === 'production') {
+
+    // Express will serve up production assets
+    // like main.js file, or main.css file!
+    app.use(express.static('client/build'));
+
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
+
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
+
+};
+
 // Read the port from the environment variable set by Heroku
 const PORT = process.env.PORT || 5000;
 
