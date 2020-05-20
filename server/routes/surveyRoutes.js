35c79eb15452d3465a7b0b64577c68acfeb3a185
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-20 13:23:56 -0400

    Set up the mailer's constructor

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
 
 const Survey = mongoose.model('surveys');
 
@@ -19,6 +21,8 @@ module.exports = app => {
             dateSent: Date.now()
 
         });
+
+        const mailer = new Mailer(survey, surveyTemplate(template));
         
     });
 };
--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -4,5 +4,16 @@ const keys = require('../config/keys');
 
 
 class Mailer extends helper.Mail {
-    
+    constructor({subject, recipients}, content) {
+        super();
+
+        //Sendgrid specific setup
+        this.from_email = new helper.Email('mhaji007@fiu.edu');
+        this.subject = subject;
+        this.body = new helper.Content('text/html', content);
+        this.recipients = this.formatAddresses(recipient); 
+
+    }
 }
+
+Module.exports = Mailer;
