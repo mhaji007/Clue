e5164e8c845f4ca8db95de5d9cc87291a5db49b2
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-19 20:00:57 -0400

    Create surveys

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,8 +1,24 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 
+const Survey = mongoose.model('surveys');
+
 module.exports = app => {
     app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+
+        const {title, subject, body, recipients} = req.body;
+
+        // Make an instance of the survey in memory
+        const survey = new Survey({
+            title,
+            subject,
+            body,
+            recipients: recipients.split(',').map (email => ({email: email.trim()})),
+            _user= req.user.id,
+            dateSent = Date.now()
+
+        });
         
     });
 };
