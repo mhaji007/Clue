78642e8bd989f3ba5c40f81a09119087c3ca6365
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-20 19:59:34 -0400

    Polish in the route handler

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -72,7 +72,7 @@ class Mailer extends helper.Mail {
             body: this.toJSON()
         });
 
-        const response = this.sgApi.API(request);
+        const response = await this.sgApi.API(request);
         return response;
     }
 }
