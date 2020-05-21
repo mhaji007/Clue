8c2e6293a4b2006acb9390e6f373c7030d412020
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-21 12:04:28 -0400

    Set up Redux Form

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -6,8 +6,8 @@ import * as actions from '../actions';
 import Header from './Header';
 import Landing from './Landing';
 import Dashboard from './Dashboard';
+import SurveyNew from './surveys/SurveyNew';
 
-const SurveyNew = () => <h2> SurveyNew</h2>
 
 class App extends Component {
 
---
+++ server/client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class SurveyNew extends Component {
+    state = {  }
+    render() { 
+        return (
+        <div>
+            SurveyNew!
+        </div> 
+         );
+    }
+}
+ 
+export default SurveyNew;
