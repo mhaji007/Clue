f8d076a05c4520a0c985fd3eb0226e90c31f02dc
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-21 00:11:25 -0400

    Add the create survey icon to the dashboard

--- server/client/src/components/Dashboard.js
+++ server/client/src/components/Dashboard.js
@@ -4,9 +4,14 @@ const Dashboard = () => {
     return (
         <div>
             Dashboard
-        </div>
+            <div className="fixed-action-btn">
+                <a className="btn-floating btn-large red">
+                    <i className="material-icons">add</i>
+                </a>
+            </div>
 
-    )
-}
+        </div>
+    );
+};
 
 export default Dashboard;
