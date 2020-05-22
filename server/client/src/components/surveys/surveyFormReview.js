641a12280fec0229c188a5f6a78c74fb1df73c21
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-22 18:17:50 -0400

    Finalize review fields

--- server/client/src/components/surveys/surveyFormReview.js
+++ server/client/src/components/surveys/surveyFormReview.js
@@ -1,13 +1,33 @@
 // SurveyFormReview shows use their form
 // inputs for review
-
+import _ from 'lodash';
 import React from 'react';
 import {connect} from 'react-redux';
+import formFields from './formFields';
 
 const SurveyFormReview = ({onCancel, formValues}) => {
+    
+    const reviewFields = _.map(formFields, ({name, label}) =>{
+        return (
+            <div key = {name}>
+            
+                <label>
+                    {label}
+                </label>
+                
+                <div>
+                
+                    {formValues[name]}
+                </div>
+            
+            </div>
+        );
+    });
+    
     return ( 
         <div>
         <h5>Please cofirm your entries</h5>
+        {reviewFields}
    
         <button onClick={onCancel} style={{borderRadius: 3}} className="black btn-flat left white-text">Back<i style={{ paddingTop:"1px"}} className="material-icons left">navigate_before</i></button>
         </div>
