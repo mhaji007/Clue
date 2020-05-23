97e463d32c6fd3273dd43bd17ef2cff0f0ffaec4
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-23 19:38:31 -0400

    Encode survey data

--- server/services/emailTemplates/surveyTemplate.js
+++ server/services/emailTemplates/surveyTemplate.js
@@ -13,11 +13,11 @@ module.exports = (survey) => {
                 <p>${survey.body}</p>
 
                 <div>
-                    <a href="${keys.redirectDomain}/api/surveys/thanks">Yes</a>
+                    <a href="${keys.redirectDomain}/api/surveys/${survey.id}/yes">Yes</a>
                 
                 </div>
                 <div>
-                    <a href="${keys.redirectDomain}/api/surveys/thanks">No</a>
+                    <a href="${keys.redirectDomain}/api/surveys/${survey.id}/no">No</a>
                     
                 </div>
             
