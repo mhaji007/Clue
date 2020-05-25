896117141cd5c0d05eceb3ba67182305ff3a4f14
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-05-25 15:20:41 -0400

    Wire React to Redux

---
+++ server/client/src/components/surveys/SurveyList.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {fetchSurveys} from '../../actions';
+
+class Surveylist extends Component {
+    componentDidMount() {
+        this.props.fetchSurveys();
+    }
+
+    render() { 
+        return ( 
+            <div>
+            
+            SurveyList
+            
+            </div>
+
+         );
+    }
+}
+
+function mapStateToProps({surveys}) {
+    return {surveys};
+    
+}
+ 
+export default connect(mapStateToProps, {fetchSurveys})(Surveylist);
